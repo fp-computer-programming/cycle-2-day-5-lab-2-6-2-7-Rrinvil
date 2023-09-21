2425a43334b109e1a7747f309729725864eb5cd0
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-09-21 12:45:34 -0400

    Add files via upload

---
+++ lab_2-7.py
@@ -0,0 +1,40 @@
+a=5
+if  (a % 2) == 0: 
+    print ('a is even.')
+else:
+    print( "a is odd.")
+if (a>=5):
+   print('a is greater than or eqaul to 5')
+elif (-5>=a): 
+    print('a is less than or eqaul to -5')
+else: print('a is between -5 and 5')
+
+b=10
+if    (b % 2) == 0: 
+    print("b is even")
+else: print("b is odd")
+if (b>=5):
+   print('b is greater than or eqaul to 5')
+elif (-5>=b): 
+    print('b is less than or eqaul to -5')
+else: print('b is between -5 and 5')
+
+c=0
+if   (c % 2) == 0: 
+    print("c is even.")
+else: print("c is odd")
+if (c>=5):
+   print('c is greater than or eqaul to 5')
+elif (-5>=c): 
+    print('c is less than or eqaul to -5')
+else: print('c is between -5 and 5')
+
+d=-4
+if  (d % 2) == 0: 
+    print("d is even.")
+else: print("d is odd.")
+if (d>=5):
+   print('d is greater than or eqaul to 5')
+elif (-5>=a): 
+    print('d is less than or eqaul to -5')
+else: print('d is between -5 and 5')
